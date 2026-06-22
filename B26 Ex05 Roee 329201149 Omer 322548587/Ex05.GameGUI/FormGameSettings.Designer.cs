5a58a5525a41230cf4f3da6388c40537e4358748
Roee Tzabari <roee.tzabari@post.runi.ac.il>
2026-06-22 20:29:26 +0300

    Add simple implementation for FormGameSettings

---
+++ B26 Ex05 Roee 329201149 Omer 322548587/Ex05.GameGUI/FormGameSettings.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Ex05.GameGUI
+{
+    partial class FormGameSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_LabelPlayers = new System.Windows.Forms.Label();
+            this.m_LabelPlayer1 = new System.Windows.Forms.Label();
+            this.m_CheckBoxPlayer2 = new System.Windows.Forms.CheckBox();
+            this.m_TextBoxPlayer1 = new System.Windows.Forms.TextBox();
+            this.m_TextBoxPlayer2 = new System.Windows.Forms.TextBox();
+            this.m_LabelBoardSize = new System.Windows.Forms.Label();
+            this.m_LabelRows = new System.Windows.Forms.Label();
+            this.m_LabelCols = new System.Windows.Forms.Label();
+            this.m_NumericUpDownRows = new System.Windows.Forms.NumericUpDown();
+            this.m_NumericUpDownCols = new System.Windows.Forms.NumericUpDown();
+            this.m_ButtonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.m_NumericUpDownRows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_NumericUpDownCols)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelPlayers
+            // 
+            this.m_LabelPlayers.AutoSize = true;
+            this.m_LabelPlayers.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_LabelPlayers.Location = new System.Drawing.Point(12, 9);
+            this.m_LabelPlayers.Name = "m_LabelPlayers";
+            this.m_LabelPlayers.Size = new System.Drawing.Size(61, 18);
+            this.m_LabelPlayers.TabIndex = 0;
+            this.m_LabelPlayers.Text = "Players:";
+            // 
+            // labelPlayer1
+            // 
+            this.m_LabelPlayer1.AutoSize = true;
+            this.m_LabelPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_LabelPlayer1.Location = new System.Drawing.Point(33, 41);
+            this.m_LabelPlayer1.Name = "m_LabelPlayer1";
+            this.m_LabelPlayer1.Size = new System.Drawing.Size(54, 15);
+            this.m_LabelPlayer1.TabIndex = 1;
+            this.m_LabelPlayer1.Text = "Player 1:";
+            // 
+            // checkBoxPlayer2
+            // 
+            this.m_CheckBoxPlayer2.AutoSize = true;
+            this.m_CheckBoxPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_CheckBoxPlayer2.Location = new System.Drawing.Point(36, 66);
+            this.m_CheckBoxPlayer2.Name = "m_CheckBoxPlayer2";
+            this.m_CheckBoxPlayer2.Size = new System.Drawing.Size(73, 19);
+            this.m_CheckBoxPlayer2.TabIndex = 2;
+            this.m_CheckBoxPlayer2.Text = "Player 2:";
+            this.m_CheckBoxPlayer2.UseVisualStyleBackColor = true;
+            this.m_TextBoxPlayer2.TextChanged += new System.EventHandler(this.checkBoxPlayer2_CheckedChanged);
+            // 
+            // textBoxPlayer1
+            // 
+            this.m_TextBoxPlayer1.Location = new System.Drawing.Point(115, 36);
+            this.m_TextBoxPlayer1.Name = "m_TextBoxPlayer1";
+            this.m_TextBoxPlayer1.Size = new System.Drawing.Size(139, 20);
+            this.m_TextBoxPlayer1.TabIndex = 3;
+            this.m_TextBoxPlayer2.TextChanged += new System.EventHandler(this.textBoxPlayer1_TextChanged);
+            // 
+            // textBoxPlayer2
+            // 
+            this.m_TextBoxPlayer2.Enabled = false;
+            this.m_TextBoxPlayer2.Location = new System.Drawing.Point(115, 65);
+            this.m_TextBoxPlayer2.Name = "m_TextBoxPlayer2";
+            this.m_TextBoxPlayer2.Size = new System.Drawing.Size(139, 20);
+            this.m_TextBoxPlayer2.TabIndex = 4;
+            this.m_TextBoxPlayer2.Text = "[Computer]";
+            this.m_TextBoxPlayer2.TextChanged += new System.EventHandler(this.textBoxPlayer2_TextChanged);
+            // 
+            // labelBoardSize
+            // 
+            this.m_LabelBoardSize.AutoSize = true;
+            this.m_LabelBoardSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_LabelBoardSize.Location = new System.Drawing.Point(12, 111);
+            this.m_LabelBoardSize.Name = "m_LabelBoardSize";
+            this.m_LabelBoardSize.Size = new System.Drawing.Size(85, 18);
+            this.m_LabelBoardSize.TabIndex = 5;
+            this.m_LabelBoardSize.Text = "Board Size:";
+            // 
+            // labelRows
+            // 
+            this.m_LabelRows.AutoSize = true;
+            this.m_LabelRows.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_LabelRows.Location = new System.Drawing.Point(33, 146);
+            this.m_LabelRows.Name = "m_LabelRows";
+            this.m_LabelRows.Size = new System.Drawing.Size(41, 15);
+            this.m_LabelRows.TabIndex = 6;
+            this.m_LabelRows.Text = "Rows:";
+            // 
+            // labelCols
+            // 
+            this.m_LabelCols.AutoSize = true;
+            this.m_LabelCols.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_LabelCols.Location = new System.Drawing.Point(153, 146);
+            this.m_LabelCols.Name = "m_LabelCols";
+            this.m_LabelCols.Size = new System.Drawing.Size(34, 15);
+            this.m_LabelCols.TabIndex = 7;
+            this.m_LabelCols.Text = "Cols:";
+            // 
+            // numericUpDownRows
+            // 
+            this.m_NumericUpDownRows.Location = new System.Drawing.Point(74, 144);
+            this.m_NumericUpDownRows.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.m_NumericUpDownRows.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.m_NumericUpDownRows.Name = "m_NumericUpDownRows";
+            this.m_NumericUpDownRows.Size = new System.Drawing.Size(39, 20);
+            this.m_NumericUpDownRows.TabIndex = 8;
+            this.m_NumericUpDownRows.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.m_NumericUpDownRows.ValueChanged += new System.EventHandler(this.numericUpDownRows_ValueChanged);
+            // 
+            // numericUpDownCols
+            // 
+            this.m_NumericUpDownCols.Location = new System.Drawing.Point(189, 144);
+            this.m_NumericUpDownCols.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.m_NumericUpDownCols.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.m_NumericUpDownCols.Name = "m_NumericUpDownCols";
+            this.m_NumericUpDownCols.Size = new System.Drawing.Size(39, 20);
+            this.m_NumericUpDownCols.TabIndex = 9;
+            this.m_NumericUpDownCols.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.m_NumericUpDownRows.ValueChanged += new System.EventHandler(this.numericUpDownCols_ValueChanged);
+            // 
+            // buttonStart
+            // 
+            this.m_ButtonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_ButtonStart.Location = new System.Drawing.Point(25, 185);
+            this.m_ButtonStart.Name = "m_ButtonStart";
+            this.m_ButtonStart.Size = new System.Drawing.Size(229, 23);
+            this.m_ButtonStart.TabIndex = 10;
+            this.m_ButtonStart.Text = "Start!";
+            this.m_ButtonStart.UseVisualStyleBackColor = true;
+            this.m_TextBoxPlayer2.TextChanged += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // FormGameSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 220);
+            this.Controls.Add(this.m_ButtonStart);
+            this.Controls.Add(this.m_NumericUpDownCols);
+            this.Controls.Add(this.m_NumericUpDownRows);
+            this.Controls.Add(this.m_LabelCols);
+            this.Controls.Add(this.m_LabelRows);
+            this.Controls.Add(this.m_LabelBoardSize);
+            this.Controls.Add(this.m_TextBoxPlayer2);
+            this.Controls.Add(this.m_TextBoxPlayer1);
+            this.Controls.Add(this.m_CheckBoxPlayer2);
+            this.Controls.Add(this.m_LabelPlayer1);
+            this.Controls.Add(this.m_LabelPlayers);
+            this.Name = "FormGameSettings";
+            this.Text = "FormGameSettings";
+            ((System.ComponentModel.ISupportInitialize)(this.m_NumericUpDownRows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_NumericUpDownCols)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label m_LabelPlayers;
+        private System.Windows.Forms.Label m_LabelPlayer1;
+        private System.Windows.Forms.CheckBox m_CheckBoxPlayer2;
+        private System.Windows.Forms.TextBox m_TextBoxPlayer1;
+        private System.Windows.Forms.TextBox m_TextBoxPlayer2;
+        private System.Windows.Forms.Label m_LabelBoardSize;
+        private System.Windows.Forms.Label m_LabelRows;
+        private System.Windows.Forms.Label m_LabelCols;
+        private System.Windows.Forms.NumericUpDown m_NumericUpDownRows;
+        private System.Windows.Forms.NumericUpDown m_NumericUpDownCols;
+        private System.Windows.Forms.Button m_ButtonStart;
+    }
+}
--- B26 Ex05 Roee 329201149 Omer 322548587/Ex05.GameGUI/FormGameSettings.cs
+++ B26 Ex05 Roee 329201149 Omer 322548587/Ex05.GameGUI/FormGameSettings.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +10,100 @@ using System.Windows.Forms;
 
 namespace Ex05.GameGUI
 {
-    public class FormGameSettings : Form
+    public partial class FormGameSettings : Form
     {
+        private string m_Player1Name;
+        public string Player1Name
+        {
+            get
+            {
+                return m_Player1Name;
+            }
+        }
+
+        private string m_Player2Name;
+        public string Player2Name
+        {
+            get
+            {
+                return m_Player2Name;
+            }
+        }
+
+        private bool m_TwoPlayers;
+        public bool TwoPlayers
+        {
+            get
+            {
+                return m_TwoPlayers;
+            }
+        }
+
+        private decimal m_NumOfRows;
+        public decimal NumOfRows
+        {
+            get
+            {
+                return m_NumOfRows;
+            }
+        }
+
+        private decimal m_NumOfCols;
+        public decimal NumOfCols
+        {
+            get
+            {
+                return m_NumOfCols;
+            }
+        }
+
+        public FormGameSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxPlayer1_TextChanged(object sender, EventArgs e)
+        {
+            m_Player1Name = (sender as TextBox).Text ?? "Player 1";
+        }
+
+        private void textBoxPlayer2_TextChanged(object sender, EventArgs e)
+        {
+            m_Player2Name = (sender as TextBox).Text ?? "Player 2";
+        }
+
+        private void checkBoxPlayer2_CheckedChanged(object sender, EventArgs e)
+        {
+            bool checkedStatus = (sender as CheckBox).Checked;
+
+            m_TwoPlayers = checkedStatus;
+            if(!checkedStatus)
+            {
+                m_Player2Name = "[Computer]";
+            }
+        }
+
+        private void numericUpDownRows_ValueChanged(object sender, EventArgs e)
+        {
+            decimal value = (sender as NumericUpDown).Value;
+
+            m_NumericUpDownCols.Value = value;
+            m_NumOfRows = value;
+            m_NumOfCols = value;
+        }
+
+        private void numericUpDownCols_ValueChanged(object sender, EventArgs e)
+        {
+            decimal value = (sender as NumericUpDown).Value;
+
+            m_NumericUpDownRows.Value = value;
+            m_NumOfRows = value;
+            m_NumOfCols = value;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
