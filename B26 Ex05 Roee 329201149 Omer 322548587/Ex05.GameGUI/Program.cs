b16c6ded6430a1b6b7145e442c1580ebdd8ed9fd
Roee Tzabari <roee.tzabari@post.runi.ac.il>
2026-06-22 12:49:19 +0300

    Add GameGUI infra-files

---
+++ B26 Ex05 Roee 329201149 Omer 322548587/Ex05.GameGUI/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05.GameGUI
+{
+    // $G$ SFN-010 (-7) When a player quits the program won't add points to the other player.
+    // $G$ SFN-012 (-7) The program does not allow another new game, after a player quits.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
